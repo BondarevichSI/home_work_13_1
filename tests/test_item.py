2b8bebe6f4745d4dd64147df582a0277898f3128
Стаснислав <stasyk.stas@mail.ru>
2023-05-25 21:22:14 +0900

    correct items and test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture()
+def product():
+    return "f", 1000, 3
+
+
+def test_calculate_total_price(product):
+    assert Item.calculate_total_price(product) == 3000
+
+
+def test_apply_discount(product):
+    assert Item.apply_discount(product) == 1000
