f5ee4b6e50299eecc5091e99708f2c6a6e58ca53
Стаснислав <stasyk.stas@mail.ru>
2023-06-06 00:51:21 +0900

    correct test and items getter, setter

--- src/item.py
+++ src/item.py
@@ -1,3 +1,9 @@
+import csv
+import os.path
+
+PATH_TO_CVS_FILE = os.path.join(os.path.dirname(__file__), "items.csv")
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +19,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all.append(self)
+        self.all.append(self.__name)
 
     def calculate_total_price(self) -> float:
         """
@@ -34,3 +40,31 @@ class Item:
         """
         self.price = self.price * self.pay_rate
         return self.price
+
+    @property
+    def name(self):
+        if len(self.__name) <= 10:
+            return self.__name
+        else:
+            raise Exception('Длина наименования товара превышает 10 символов')
+
+
+    @name.setter
+    def name(self, data):
+        self.__name = data
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open(PATH_TO_CVS_FILE, newline='') as csvfile:
+            csv_data = csv.DictReader(csvfile)
+            for row in csv_data:
+                name, price, quantity = row.values()
+                price = cls.string_to_number(price)
+                quantity = cls.string_to_number(quantity)
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(num):
+        if "." in num:
+            return float(num)
+        return int(num)
--- tests/test_item.py
+++ tests/test_item.py
@@ -5,7 +5,7 @@ from src.item import Item
 
 @pytest.fixture()
 def product():
-    return Item("f", 1000, 3)
+    return Item('Смартфон', 1000, 3)
 
 
 def test_calculate_total_price(product):
@@ -14,3 +14,18 @@ def test_calculate_total_price(product):
 
 def test_apply_discount(product):
     assert product.apply_discount() == 1000
+
+
+def test_name(product):
+    assert product.name == 'Смартфон'
+
+
+def test_name_setter(product):
+    product.name = 'СуперСмартфон'
+    with pytest.raises(Exception):
+        product.name(0)
+
+
+def test_string_to_number(product):
+    assert product.string_to_number('5') == 5
+    assert product.string_to_number('5.5') == 5.5
